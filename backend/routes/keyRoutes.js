3f4690072222fcbb7528a5262e76181f5fdbc232
Ahmed527 <naveedahmed151106@gmail.com>
2025-10-02 19:00:51 +0530

    PRODUCTION PHASE

--- backend/routes/keyRoutes.js
+++ backend/routes/keyRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const crypto = require("crypto");
-const { default: verifyClerkOidc } = require("../middleware/verifyClerkOidc");
+const verifyClerkOidc  = require("../middleware/verifyClerkOidc");
 const UserKey = require("../models/UserKey");
 
 const kidOf = (pubB64) =>
