3aba8dd66270d7bdc0cf22d9f7f5d60fd51dfe45
Ahmed527 <naveedahmed151106@gmail.com>
2025-11-07 19:05:03 +0530

    group logic error3 fixed (V2)

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -10,11 +10,29 @@ const GroupSchema = new mongoose.Schema(
       required: true,
       index: true,
     },
-    userIds: { type: [mongoose.Schema.Types.ObjectId], default: [] },
+    userIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     lockIds: { type: [Number], default: [] },
   },
-  { timestamps: true }
+  { 
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
 );
+
+GroupSchema.virtual('users', {
+  ref: 'User',
+  localField: 'userIds',
+  foreignField: '_id',
+  justOne: false
+});
+
+GroupSchema.virtual('locks', {
+    ref: 'Lock',
+    localField: 'lockIds',
+    foreignField: 'lockId',
+    justOne: false
+});
 
 GroupSchema.index({ workspace_id: 1, name: 1 }, { unique: true });
 
